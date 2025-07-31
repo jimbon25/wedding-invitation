d6c5a8e5239bf7fa879ccf7cfdf7dac087e1fdbf
jimbon25 <jimbonluis90@gmail.com>
2025-07-31 09:38:39 +0700

    Add animated SVG divider between RSVP and GuestBook, and confetti/check animation on successful RSVP/GuestBook submit

--- src/components/RSVPAndGuestBook.tsx
+++ src/components/RSVPAndGuestBook.tsx
@@ -11,12 +11,20 @@ const RSVPAndGuestBook: React.FC = () => {
         <div className="col-12 col-lg-6 d-flex flex-column">
           <RSVPForm />
         </div>
-        {/* Garis pemisah */}
+        {/* Divider animasi SVG */}
         <div className="d-none d-lg-flex col-lg-1 justify-content-center align-items-stretch p-0">
-          <div style={{ width: '2px', background: '#e0e0e0', height: '100%' }} />
+          <svg width="2" height="100%" style={{minHeight:'320px',height:'100%'}} viewBox="0 0 2 320">
+            <rect x="0" y="0" width="2" height="320" rx="1" fill="#e0e0e0">
+              <animate attributeName="height" from="0" to="320" dur="1.1s" fill="freeze" />
+            </rect>
+          </svg>
         </div>
-        <div className="d-block d-lg-none col-12">
-          <hr style={{ borderTop: '2px solid #e0e0e0', margin: '32px 0' }} />
+        <div className="d-block d-lg-none col-12" style={{textAlign:'center'}}>
+          <svg width="120" height="12" viewBox="0 0 120 12" style={{overflow:'visible',margin:'32px 0'}}>
+            <rect x="0" y="5" width="120" height="2" rx="1" fill="#e0e0e0">
+              <animate attributeName="width" from="0" to="120" dur="1.1s" fill="freeze" />
+            </rect>
+          </svg>
         </div>
         <div className="col-12 col-lg-5 d-flex flex-column">
           <GuestBook />
